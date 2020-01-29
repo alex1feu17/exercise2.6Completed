a34da75c186bff06cac92112c0a1a0f0e1377584
alex1feu17 <alexyd@hotmail.ca>
2020-01-28 19:55:53 -0500

    exercise2.5

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exercise2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tableau1 = new int[] { 1,3,-5,4 };
+            int[] tableau2 = new int[] { 1,-4,-5,-2};
+
+            int[] tableaureponse = {0,0,0,0 };
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                tableaureponse[i] = tableau1[i] * tableau2[i];
+                Console.WriteLine(tableaureponse[i]);
+            }
+
+           
+        }
+    }
+}
